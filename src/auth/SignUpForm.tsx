5a13b2815f136d71ae6f4cc451e2c15adcc07055
Szilvia Csernusne Berczes <csernus.szilvi@gmail.com>
2023-11-18 19:01:20 +0000

    style register form

--- src/auth/SignUpForm.tsx
+++ src/auth/SignUpForm.tsx
@@ -1,10 +1,11 @@
+import classes from './Auth.module.css';
+import btnClasses from '../components/Button.module.css'
+
 import { Link } from 'react-router-dom';
 
-import classes from './Auth.module.css';
-
-import { Col, Row} from 'react-bootstrap';
+import { Col, Row, Form, Button } from 'react-bootstrap';
 import Card from 'react-bootstrap/Card';
-import illustration from '../assets/register.svg'
+import illustration from '../assets/register.svg';
 
 const SignUpForm = () => {
 	return (
@@ -18,12 +19,50 @@ const SignUpForm = () => {
 				/>
 			</Col>
 			<Col className="my-auto py-2 p-md-2" md={6}>
-				<Card className ={classes.authCard}>
+				<Card className={`p-4 ${classes.authCard}`}>
 					<Card.Body>
 						<div>
-							<h1 className={classes.authHeader}>register</h1>
-
+							<h1 className={`pb-3 ${classes.authHeader}`}>register</h1>
 						</div>
+
+						<Form>
+							<Form.Group className="mb-3" controlId="email">
+								<Form.Label className="d-none">Email address</Form.Label>
+								<Form.Control
+									className={classes.authInput}
+									type="email"
+									placeholder="Enter email"
+									name="email"
+								/>
+							</Form.Group>
+
+							<Form.Group className="mb-3" controlId="password1">
+								<Form.Label className="d-none">Password</Form.Label>
+								<Form.Control
+									className={classes.authInput}
+									type="password"
+									placeholder="Password"
+									name="password1"
+								/>
+							</Form.Group>
+
+							<Form.Group className="mb-3" controlId="password2">
+								<Form.Label className="d-none">Password (again)</Form.Label>
+								<Form.Control
+									className={classes.authInput}
+									type="password"
+									placeholder="Password (again)"
+									name="password2"
+								/>
+							</Form.Group>
+
+							<Button
+								className={`${btnClasses.button} ${btnClasses.wide}`}
+								type="submit"
+							>
+								Register
+							</Button>
+						</Form>
 						<div>
 							Already have an account?
 							<Link className={classes.authLink} to="/signin">

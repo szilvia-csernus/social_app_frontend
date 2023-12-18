4e111f1e0179a78c5168abc044b34d7f8cb44460
Szilvia Csernusne Berczes <csernus.szilvi@gmail.com>
2023-12-18 11:48:50 +0000

    reset baseURL and related test cases

--- src/api/axiosDefaults.ts
+++ src/api/axiosDefaults.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-axios.defaults.baseURL = 'http://127.0.0.1:8000/';
+axios.defaults.baseURL =
+	'https://social-app-szilvia-8965907d743d.herokuapp.com/';
 // axios.defaults.headers.post['Content-Type'] = 'multipart/form-data';
 axios.defaults.headers.post['Content-Type'] = 'application/json';
 axios.defaults.withCredentials = true; // to avoid any CORS errors when sending COOKIES
--- src/mocks/handler.js
+++ src/mocks/handler.js
@@ -1,18 +1,8 @@
 import { rest } from "msw";
 
-const baseURL = 'http://127.0.0.1:8000/';
+const baseURL = 'https://social-app-szilvia-8965907d743d.herokuapp.com/';
 
 export const handlers = [
-	rest.get(`${baseURL}api/token/`, (req, res, ctx) => {
-		return res(
-			ctx.json({
-				refresh:
-					'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoicmVmcmVzaCIsImV4cCI6MTcwMjgyOTQwNCwiaWF0IjoxNzAyNzQzMDA0LCJqdGkiOiI1Y2RkYzI2MWU4ODc0NmRhOTRiOTRhYzc5NzUzZGYyMSIsInVzZXJfaWQiOjR9.UvCJe8Oto_k4ahgLGTMSKY-7oKC4RdqWjLwH4nZl-Uk',
-				access:
-					'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzAyNzQzMDY0LCJpYXQiOjE3MDI3NDMwMDQsImp0aSI6IjY0NGMxZmVlYWJmODQ0MTRiN2UyODExYzg3ZWM3NDA2IiwidXNlcl9pZCI6NH0.qsh94PbnwNO-DyfOpYcYg8WnJioACMmT6J2JoHtGj3U',
-			})
-		);
-	}),
 	rest.get(`${baseURL}dj-rest-auth/user`, (req, res, ctx) => {
 		return res(
 			ctx.json({
@@ -30,15 +20,28 @@ export const handlers = [
 	rest.get(`${baseURL}dj-rest-auth/login/`, (req, res, ctx) => {
 		return res(
 			ctx.json({
-				key: '94e0ae0d74e4ea779399339589f209b2502ff841',
+				access:
+					'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzAyODU1NDE2LCJpYXQiOjE3MDI4NTM2MTYsImp0aSI6IjhjMWJhZGEzZTk3ZDRiMjY4OGQwNDYxNjI2YjhjYWI5IiwidXNlcl9pZCI6NH0.dau6NgOq1Gj9x43yXuhBO3zL1wq5p4JwEVddGRVc924',
+				refresh: '',
+				user: {
+					pk: 4,
+					username: 'test2',
+					email: 'test2@gmail.com',
+					first_name: '',
+					last_name: '',
+					profile_id: 4,
+					profile_image:
+						'https://res.cloudinary.com/domjz4dz6/image/upload/v1/media/../default_profile_bq1aht',
+				},
 			})
 		);
 	}),
-	rest.post(`${baseURL}api/token/refresh/`, (req, res, ctx) => {
+	rest.post(`${baseURL}dj-rest-auth/token/refresh/`, (req, res, ctx) => {
 		return res(
 			ctx.json({
 				access:
-					'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzAyNzQzNjEyLCJpYXQiOjE3MDI3NDMzNTMsImp0aSI6ImQxOGVkMzAwNTU1ZjQ2MTE5ZjU4ODdkZGQ5M2VjNjQ0IiwidXNlcl9pZCI6NH0.cQNxfZm1689D1wxCsEcquEPJh58eyoyWZ3DQSbf6-yc',
+					'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzAyODkzMDgwLCJpYXQiOjE3MDI4NTM2MTYsImp0aSI6IjZlN2Y3Y2JjOTE0ZDQ3Yjc5YjMwNzA2ZDRjZDIwZmNiIiwidXNlcl9pZCI6NH0.l-euJw-KdpkAl_vyRLQ-sqoMqgOHsPi-uF71WMNjdBM',
+				access_expiration: '2023-12-18T09:51:20.389275Z',
 			})
 		);
 	}),
